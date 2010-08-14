9617c4b49096d737a5572b68e5257984485a503d
Brett Stimmerman <brettstimmerman@gmail.com>
2010-08-14 15:22:23 -0700

    tidy up

--- src/outside-events.js
+++ src/outside-events.js
@@ -11,7 +11,7 @@
  * @module gallery-outside-events
  */
 
-YUI.add('gallery-outside-events', function(Y) {
+YUI.add('gallery-outside-events', function (Y) {
 
 /**
  * Outside events are synthetic DOM events that fire when a corresponding native
@@ -41,44 +41,39 @@ var nativeEvents = [
  * @param {String} name (optional) custom outside event name
  */
 Y.Event.defineOutside = function (event, name) {
-    
     name = name || event + 'outside';
     
     Y.Event.define(name, {
         
         on: function (node, sub, notifier) {
-            
-            sub.onHandle = Y.one('doc').on(event, function(e){
-            	if (this.isOutside(node, e.target)){
-            		notifier.fire(e);
-            	}
+            sub.onHandle = Y.one('doc').on(event, function(e) {
+                if (this.isOutside(node, e.target)) {
+                    notifier.fire(e);
+                }
             }, this);
         },
         
         detach: function (node, sub, notifier) {
-            
             sub.onHandle.detach();
         },
         
         delegate: function (node, sub, notifier, filter) {
-            
-            sub.delegateHandle = Y.one('doc').delegate(event, function(e){
+            sub.delegateHandle = Y.one('doc').delegate(event, function (e) {
                 if (this.isOutside(node, e.target)) {
                     notifier.fire(e);
                 }
             }, filter, this);
         },
         
-        detachDelegate : function (node, sub, notifier, filter) {
-            
+        detachDelegate: function (node, sub, notifier, filter) {
             sub.delegateHandle.detach();
         },
         
         isOutside: function (node, target) {
-            
-            return target !== node && ! target.ancestor(function(p){ return p === node; });
+            return target !== node && !target.ancestor(function (p) {
+                    return p === node;
+                });
         }
-        
     });
 };
 
@@ -87,5 +82,4 @@ Y.each(nativeEvents, function (event) {
     Y.Event.defineOutside(event);
 });
 
-
-}, '1.1.0' ,{requires:['event-focus', 'event-synthetic']});
+}, '1.1.0', { requires: ['event-focus', 'event-synthetic'] });
